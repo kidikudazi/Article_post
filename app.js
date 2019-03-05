ff9f4ee21f500d1608d5345838d86901c122ab48
Francis Abikoye Adeboye <francisadeboye5@gmail.com>
2019-03-05 12:10:08 +0100

    Final update

--- app.js
+++ app.js
@@ -36,16 +36,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'pug');
 
-// if (app.get('env') === 'production') {
-//     app.use(function (err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-//     console.log('done');
-// }
 
 // body parser Middleware parse application
 app.use(bodyParser.urlencoded({extended: false }));
